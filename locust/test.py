4be1301fec28619c08fda610cd04ebc5922b8554
Sajad Faghfoor Maghrebi <sajad.faghfoor@gmail.com>
2021-10-26 19:26:55 +0330

    locust updated

--- locust/test.py
+++ locust/test.py
@@ -1,18 +1,22 @@
 from random import paretovariate
-import time
 from locust import task, between, HttpUser
 from locust.user import wait_time
 
 class WebsiteUser(HttpUser):
     def __init__(self, parent):
         super(WebsiteUser, self).__init__(parent)
-        self.urlNode = '/'
+        self.urlNode = '/node/sha256'
+        self.urlGo = '/go/sha256'
 
     wait_time = between(1, 5)
 
     @task
     def get_nodejs(self):
         self.client.get(self.urlNode)
+
+    @task
+    def get_golang(self):
+        self.client.get(self.urlGo)
 
     # @task
     # def set_nodejs(self):
