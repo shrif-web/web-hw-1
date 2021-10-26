933e914608a6e76dc3d0a4922e1ed87ef2985a08
Sajad Faghfoor Maghrebi <sajad.faghfoor@gmail.com>
2021-10-26 20:21:45 +0330

    locust input handled

--- locust/test.py
+++ locust/test.py
@@ -1,24 +1,37 @@
 from random import paretovariate
 from locust import task, between, HttpUser
 from locust.user import wait_time
+import random
+import time
+import string
+
+def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 class WebsiteUser(HttpUser):
     def __init__(self, parent):
         super(WebsiteUser, self).__init__(parent)
         self.urlNode = '/node/sha256'
-        self.urlGo = '/go/sha256'
+        self.urlGolang = '/go/sha256'
 
-    wait_time = between(1, 5)
-
-    @task
-    def get_nodejs(self):
-        self.client.get(self.urlNode)
+    wait_time = wait_time.constant(1)
 
     @task
     def get_golang(self):
-        self.client.get(self.urlGo)
+        key = id_generator()
+        self.client.get(self.urlGolang + f"/hashedString?hashedString={key}")
 
-    # @task
-    # def set_nodejs(self):
-    #     url = 'node/sha256'
-    #     pass
+    @task
+    def get_nodejs(self):
+        key = id_generator()
+        self.client.get(self.urlNode + f"/hashedString?hashedString={key}")
+
+    @task
+    def post_golang(self):
+        sha = id_generator()
+        self.client.post(self.urlGolang, json={'str': sha})
+
+    @task
+    def post_golang(self):
+        sha = id_generator()
+        self.client.post(self.urlNode, json={'str': sha})
